9923407d4c62e89882b62e65c5caa020cdc37e93
AnthoR100 <anthony.hersant@outlook.fr>
2026-05-21 16:11:39 +0100

    init: spring boot project scaffolding

---
+++ src/main/java/fr/ekod/cda/ja/ekod_room_booking/EkodRoomBookingApplication.java
@@ -0,0 +1,13 @@
+package fr.ekod.cda.ja.ekod_room_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkodRoomBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EkodRoomBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/ekod/cda/ja/ekod_room_booking/EkodRoomBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ekod.cda.ja.ekod_room_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkodRoomBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
